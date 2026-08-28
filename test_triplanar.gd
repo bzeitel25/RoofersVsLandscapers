ba8cb984deb90055341ef924669a69f85d13990c
BZ <roofers_vs_landscapers@example.com>
2026-08-27 20:54:49 -0400

    Cleanup temp file

--- test_triplanar.gd
+++
@@ -1,9 +0,0 @@
-﻿extends SceneTree
-func _init():
-	var m = StandardMaterial3D.new()
-	var keys = m.get_property_list().map(func(p): return p.name)
-	var file = FileAccess.open("res://props.txt", FileAccess.WRITE)
-	file.store_string(",".join(keys))
-	file.close()
-	quit()
-
